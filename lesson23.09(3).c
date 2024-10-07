66e074b142b52f3af21cdaeee4f5da0c8e233f2e
appleaww <artemmorozz2006@gmail.com>
2024-10-07 22:19:16 +0300

    Update lesson23.09(3).c

--- lesson23.09(3).c
+++ lesson23.09(3).c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 int main() {
-    for (int i =0;i <10;i++){
-        printf("%d\n",i);
+    int arr[10];
+    
+ 
+    for(int i = 0; i<10; i++){
+         scanf("%d", &arr[i]);
     }
+    int chet = 0;
+    int ne_chet =0;
+    for (int a =0; a<10;a++) {
+        if (arr[a] %2 == 0) {
+            chet ++;
+        }
+        else{
+            ne_chet ++;
+        }
+    }  
+    printf("%d %d", chet, ne_chet);
+
+    
+    return 0;
 }
