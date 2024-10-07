3d88c5a05418e0e66274b5da11787ff4c8be1af1
appleaww <artemmorozz2006@gmail.com>
2024-10-07 11:10:26 +0300

    Update lesson23.09.c

--- lesson23.09.c
+++ lesson23.09.c
@@ -3,15 +3,7 @@ int main() {
     int num;
     int k =0;
     scanf("%d",&num);
-    if (num/10%2==0)
-    k++;
-    if (num/100%2==0)
-    k++;
-    if (num/1000%2==0)
-    k++;
-    if (num/10000%2==0)
-    k++;
-    printf("%d", k);
-   
+    
+    printf("%d", num%1000/100);
     return 0;
 }
