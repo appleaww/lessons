eea35d3c4c5cb86700d61971d0fbc6b19ead7b93
appleaww <artemmorozz2006@gmail.com>
2024-10-21 14:00:32 +0300

    Create fvbdfjbvkdjb.c

---
+++ fvbdfjbvkdjb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mm_malloc.h"
+int main() {
+    int arr[4][4];
+    for(int i =0 ; i<4; i++){
+        for(int j = 0; j<4; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    
+    for(int i =0 ; i<4; i++){
+        for(int j = 0; j<4; j++){
+
+            
+        }
+    }
+    
+return 0;
+}
